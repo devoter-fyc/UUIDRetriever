d0368d4b7099aa4dd0f0cb7e8f52db2c50ef1405
devoter-fyc <fycsfls_winter@126.com>
2025-04-27 22:41:51 +0800

    Initial commit.

---
+++ background/server.py
@@ -0,0 +1,74 @@
+import json
+import os
+import time
+import sys
+import socket
+import threading as thread
+__author__ = "devoter-fyc"
+####################
+# Define JSON reading and writing functions
+def read_from_file(file_path: str):
+    if os.path.exists(file_path):
+        with open(file_path, 'r+') as file:
+            data = json.load(file)
+        return data
+    else:
+        raise FileNotFoundError(f"File {file_path} not found.")
+        
+# Main code
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if len(sys.argv) < 3:
+    s.bind(('localhost', 8192))
+    s.listen(8)
+    print("Server started on port 8192")
+else:
+    s.bind(('localhost', sys.argv[2]))
+    s.listen(8)
+    print(f"Server started on port {sys.argv[2]}")
+
+# Open the JSON file
+if len(sys.argv) < 2:
+    print("Specify JSON file as first argument.")
+    raise FileNotFoundError
+else:
+    path = sys.argv[1]
+
+def handle_client(sock: socket.socket, addr: socket._RetAddress):
+    print(f"Connection from {addr} has been established.")
+    sock.send(b"Welcome.")
+    while True:
+        try:
+            data = sock.recv(2048).decode()
+            if not data or data == 'exit':
+                print("Exit request received, exiting.")
+                break
+            x = read_from_file(path)
+            if isinstance(x, dict):
+                t = x[data]
+                sending = json.dumps(t).encode("utf-8")
+                sock.send(sending)
+            else:
+                print("Data format is unexpected, halting.")
+                break
+        except FileNotFoundError:
+            print("File is not found.")
+            sock.send(b"File is not found.")
+            return 1
+        except json.JSONDecodeError:
+            print("JSON format is illegal.")
+            sock.send(b"JSON format is illegal.")
+            return 10
+        except IOError as e:
+            print(f"Caught IOError: {e}")
+            return -1
+        except Exception as e:
+            print(f"Unknown error occurred: {e}")
+            return -1
+        sock.close()
+        print(f"Connection from {addr} closed.")
+        return 0
+
+while True:
+    sock, addr = s.accept()
+    t = thread.Thread(target=handle_client, args=(sock, addr))
+    t.start()
---
+++ io/fyc/uuid/parser/Client.java
@@ -0,0 +1,73 @@
+package io.fyc.uuid.parser;
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.rmi.RemoteException;
+
+public class Client {
+    public static void main(String[] args) throws Exception{
+       var h = new Handler(new Socket(args[0], Integer.parseInt(args[1])), args[2]);
+       h.run();
+       System.out.println("Test done.");
+    }
+}
+
+class Handler {
+    protected Socket sock;
+    private String someUUID;
+
+    public Handler(Socket sock, String someUUID) {
+        this.sock = sock;
+        this.someUUID = someUUID;
+    }
+
+    public String getSomeUUID() {
+        return someUUID;
+    }
+
+    public void setSomeUUID(String someUUID) {
+        this.someUUID = someUUID;
+    }
+
+    public String run() throws IOException{
+        try (InputStream input = this.sock.getInputStream()) {
+            try (OutputStream output = this.sock.getOutputStream()) {
+                return handle(input, output);
+            }
+        } catch (Exception e) {
+            try {
+                this.sock.close();
+            } catch (IOException q) {
+                // DO NOTHING AT ALL
+            }
+        }
+        System.out.println("Client disconnected.");
+        return "";
+    }
+
+    private String handle(InputStream input, OutputStream output) throws IOException {
+        var writer = new BufferedWriter(new OutputStreamWriter(output, StandardCharsets.UTF_8));
+        var reader = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
+        String s = reader.readLine();
+        if (!s.equals("Welcome.")) {
+            System.err.println("Server is incorrectly setup.");
+            throw new SocketException();
+        }
+        if (this.someUUID.equals("")) {
+            System.err.println("UUID is not given. Can't send request.");
+            throw new IllegalArgumentException();
+        }
+        writer.write(this.someUUID);
+        writer.flush();
+        String response = reader.readLine();
+        if (response.equals("File is not found.")) {
+            System.err.println("File not found, quitting.");
+            throw new FileNotFoundException();
+        }
+        if (response.equals("JSON format is illegal.")) {
+            System.err.println("JSON format is illegal, quitting.");
+            throw new RemoteException();
+        }
+        return response;
+    }
+}
